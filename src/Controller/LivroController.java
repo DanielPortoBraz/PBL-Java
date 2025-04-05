6c914a31eb85eb75fb2724abea5f4830b2261dd3
DanielPortoBraz <danielpb132@gmail.com>
2025-04-05 18:20:23 -0300

    "Métodos de busca para Livro"

--- src/Controller/LivroController.java
+++ src/Controller/LivroController.java
@@ -22,6 +22,36 @@ public class LivroController extends RegistroController{
         livrosR.addLivro(new Livro(titulo, generos, anoLancamento, visto, autor, editora, isbn, exemplar));
     }
 
+    public void buscarLivros(int categoria, String filtro){
+
+        switch(categoria){
+            case 1: // Titulo
+                livrosR.buscarTitulo(filtro).forEach(System.out::println);
+                break;
+
+            case 2: // Autor
+                livrosR.buscarAutor(filtro).forEach(System.out::println);
+                break;
+
+            case 3: // Gênero
+                //livrosR.buscarGenero(filtro).forEach(System.out::println);
+                break;
+
+            case 4: // Ano
+                int filtroNum = Integer.parseInt(filtro);
+                livrosR.buscarAno(filtroNum).forEach(System.out::println);
+                break;
+
+            case 5: // Isbn
+                System.out.println(livrosR.buscarIsbn(filtro));
+                break;
+
+            default:
+                System.out.println("Não encontrado.");
+
+        }
+    }
+
     public void listarLivros(){
         for (Livro i : livrosR.getLivros()){
             System.out.println(i.toString());
--- src/Model/LivroRepositorio.java
+++ src/Model/LivroRepositorio.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class LivroRepositorio {
     private TreeSet<Livro> livros;
@@ -19,5 +20,36 @@ public class LivroRepositorio {
 
     public TreeSet<Livro> getLivros() {
         return livros;
+    }
+
+    public TreeSet<Livro> buscarTitulo(String titulo){
+        return this.livros.stream()
+                .filter(livro -> livro.getTitulo().toLowerCase().contains(titulo.toLowerCase()))
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public TreeSet<Livro> buscarAutor(String autor){
+        return this.livros.stream()
+                .filter(livro -> livro.getAutor().toLowerCase().contains(autor.toLowerCase()))
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public TreeSet<Livro> buscarGenero(Genero genero){
+        return this.livros.stream()
+                .filter(livro -> livro.getGenero().contains(genero))
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public TreeSet<Livro> buscarAno(int ano){
+        return this.livros.stream()
+                .filter(livro -> livro.getAnoLancamento() == ano)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    public Livro buscarIsbn(String isbn){
+        for (Livro i : livros){
+            if (i.getIsbn().equalsIgnoreCase(isbn)) return i;
+        }
+        return null;
     }
 }
