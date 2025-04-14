8911d057560bd6bbc0a2c99712d01923b627d174
DanielPortoBraz <danielpb132@gmail.com>
2025-04-13 23:23:33 -0300

    "Atributos de nome para Gênero, permite acentos e espaço"

--- src/Model/Genero.java
+++ src/Model/Genero.java
@@ -1,23 +1,38 @@
 package Model;
 
 public enum Genero {
-    ACAO,
-    AVENTURA,
-    ANIMACAO,
-    BIOGRAFIA,
-    COMEDIA,
-    DOCUMENTARIO,
-    DRAMA,
-    FANTASIA,
-    FICCAO_CIENTIFICA,
-    GUERRA,
-    HISTORICO,
-    MUSICAL,
-    MISTERIO,
-    POLICIAL,
-    ROMANCE,
-    SUSPENSE,
-    TERROR,
-    THRILLER,
-    FAROESTE,
+    ACAO("Ação"),
+    AVENTURA("Aventura"),
+    ANIMACAO("Animação"),
+    BIOGRAFIA("Biografia"),
+    COMEDIA("Comédia"),
+    DOCUMENTARIO("Documentário"),
+    DRAMA("Drama"),
+    FANTASIA("Fantasia"),
+    FICCAO_CIENTIFICA("Ficção Científica"),
+    GUERRA("Guerra"),
+    HISTORICO("Histórico"),
+    MUSICAL("Musical"),
+    MISTERIO("Mistério"),
+    POLICIAL("Policial"),
+    ROMANCE("Romance"),
+    SUSPENSE("Suspense"),
+    TERROR("Terror"),
+    THRILLER("Thriller"),
+    FAROESTE("Faroeste");
+
+    private final String nomeFormatado;
+
+    Genero(String nomeFormatado) {
+        this.nomeFormatado = nomeFormatado;
+    }
+
+    public String getNomeFormatado() {
+        return nomeFormatado;
+    }
+
+    @Override
+    public String toString() {
+        return nomeFormatado;
+    }
 }
