162c465c4e74adb825d33606ce6c6d171734f422
DanielPortoBraz <danielpb132@gmail.com>
2025-04-22 19:32:35 -0300

    "Implementação da Main."

---
+++ test/src/main/java/DiarioCultural.java
@@ -0,0 +1,22 @@
+import Model.Genero;
+import Model.Temporada;
+import Controller.*;
+import View.*;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class DiarioCultural {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LivroController livroController = new LivroController();
+        FilmeController filmeController = new FilmeController();
+        SerieController serieController = new SerieController();
+
+        MenuPrincipal menu = new MenuPrincipal(scanner, livroController, filmeController, serieController);
+        menu.exibir();
+
+        scanner.close();
+    }
+}
