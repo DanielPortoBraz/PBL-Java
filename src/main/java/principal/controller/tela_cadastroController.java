4a8aae96879ed4b505b624639a408784c523be2d
DanielPortoBraz <danielpb132@gmail.com>
2025-06-16 22:02:20 -0300

    "Tela de Cadastro de livro"

---
+++ src/main/java/principal/controller/tela_cadastroController.java
@@ -0,0 +1,26 @@
+package principal.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import principal.DiarioCultural;
+
+public class tela_cadastroController implements Initializable {
+
+    @FXML
+    private Button bt_retornar;
+
+    @FXML
+    void clicarRetornar(ActionEvent event) {
+        DiarioCultural.changeScene("principal");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
